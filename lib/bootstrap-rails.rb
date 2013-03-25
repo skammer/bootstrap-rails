75c8ff1ef82920a2d1737b4baadcef06194ec991
Max Vasiliev <max@vasiliev.me>
2013-03-25 16:58:38 +0400

    Add #to_s method to rails version

--- lib/bootstrap-rails.rb
+++ lib/bootstrap-rails.rb
@@ -4,7 +4,7 @@ require "bootstrap-rails/version"
 module Bootstrap
   module Rails
     require "bootstrap-rails/ie_hex_str"
-    if ::Rails.version < "3.1"
+    if ::Rails.version.to_s < "3.1"
       require "bootstrap-rails/railtie"
     else
       require "bootstrap-rails/engine"
